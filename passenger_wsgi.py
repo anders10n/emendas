943602bd768e9b45c7d2bf2bf9cd978ba34f4c70
Anderson <145286198+anders10n@users.noreply.github.com>
2026-04-08 15:57:46 -0300

    Mensagem

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Adiciona o diretório da aplicação ao path do Python
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Importa o objeto 'app' do seu arquivo app.py e passa para o Passenger via variável 'application'
-from app import app as application
